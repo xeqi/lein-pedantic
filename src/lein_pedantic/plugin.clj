b6c2ffaca5008b4fbe8b5de4b7e4a300e92b61de
Nelson Morris <nmorris@nelsonmorris.net>
2012-08-30 15:24:45 -0500

    Better error message formatting and help message

--- src/lein_pedantic/plugin.clj
+++ src/lein_pedantic/plugin.clj
@@ -4,21 +4,37 @@
             [pedantic.core :as pedantic]
             [clojure.string :as string]))
 
+(defn help-message [used desired]
+  (let [name (first (last used))]
+    (cond (= (count used) 1)
+          (str "Please use "
+               (conj (first desired) :exclusions [name])
+               " to get " (last used)
+               " or remove " (first used)
+               " to get " (last desired) ".")
+          (= (count desired) 1)
+          (str "Please use "
+               (conj (first used) :exclusions [name])
+               " to get " (last desired)
+               " or remove " (first desired)
+               " to get " (last used) ".")
+          :else
+          (str "Please use "
+               (conj (first used) :exclusions [name])
+               " to get " (last desired)
+               " or use "
+               (conj (first desired) :exclusions [name])
+               " to get " (last used) "."))))
+
 (defn create-msg [overrulled]
   (let [nl (System/getProperty "line.separator")]
-    (string/join 
-     (for [[used desired] overrulled]
-       (let [name (first (last used))]
-         (str "Failing dependency resolution because" nl
-              (string/join " -> " used) nl
-              "is overrulling" nl
-              (string/join " -> " desired) nl
-              "Please use "
-              (conj (first used) :exclusions [name])
-              " to get " (last desired)
-              " or use "
-              (conj (first desired) :exclusions [name])
-              " to get " (last used) "."))))))
+    (str "Failing dependency resolution because:" nl nl
+         (string/join (str nl nl)
+                      (for [[used desired] overrulled]
+                        (str (string/join " -> " used) nl
+                               "  is overrulling" nl
+                               (string/join " -> " desired) nl nl
+                               (help-message used desired)))))))
 
 
 (defn pedantic-deps [f & args]
